768c6567a38e52936b046b48049f5a8636a7c646
aakk11220066 <45775044+aakk11220066@users.noreply.github.com>
2020-10-06 11:13:36 +0300

    Correction to Barrier - latest version

--- Barrier.cpp
+++ Barrier.cpp
@@ -2,39 +2,207 @@
 // Created by Akiva on 2020-06-01.
 //
 
+//barrier.cpp
 #include "Barrier.h"
+
+#define DEBUG_PRINT(errmsg) /*std::cerr << errmsg << std::endl*/ //DEBUG
 
 void Barrier::wait() {
     barrierAccessLock.lock();
-
+	
+	//check if permitted access to Barrier
+	if (!isOpen()) {
+		DEBUG_PRINT("Waiting for barrier to reopen");
+		waitingQueue.cond_wait();
+	}
+	
     if (!isFull()) { //sort thread by type - sleeper type thread or waker type thread
         //if not full yet - go to sleep until fills up
-
-        cond_beds[nextEmptyBed++].cond_wait(); //unlocks lock
+		unsigned int selectedBed = nextEmptyBed++;
+		DEBUG_PRINT("Going to sleep on bed number "<<selectedBed);
+        cond_beds[selectedBed].cond_wait(); //unlocks lock
+        
+        //upon waking up, 
+        nextEmptyBed--;
+        
+        //check if last one to exit and if so - reset
+        if (nextEmptyBed == 0) reset();
     } else {
+		//close barrier until emptied
+		_isOpen = false;
         //signal everyone to wake up
         for (ConditionVariable &cond_bed : cond_beds) {
+			DEBUG_PRINT("Signalling");
             cond_bed.cond_signal();
         }
-        reset();
     }
     barrierAccessLock.unlock();
 }
 
 Barrier::Barrier(unsigned int num_of_threads) :
         numOfThreads(num_of_threads),
-        //cond_beds(std::vector<ConditionVariable>(numOfThreads - 1, ConditionVariable(&barrierAccessLock))) {}
         cond_beds(std::vector<ConditionVariable>(num_of_threads - 1)) {
 
     for (ConditionVariable &cond_bed : cond_beds) cond_bed.setLock(&barrierAccessLock);
+    waitingQueue.setLock(&barrierAccessLock);
 }
 
 bool Barrier::isFull() {
     if (!barrierAccessLock.isLocked()) throw LockNotAcquired();
-    return nextEmptyBed == numOfThreads - 1;
+    return nextEmptyBed == (numOfThreads-1);
+}
+
+bool Barrier::isOpen() {
+	if (!barrierAccessLock.isLocked()) throw LockNotAcquired();
+    return _isOpen;
 }
 
 void Barrier::reset() {
-    nextEmptyBed = 0; //already is atomic
+	DEBUG_PRINT("Resetting, currently nextEmptyBed is "<<nextEmptyBed);
+    _isOpen = true;
+    //wake everyone in waitingQueue 
+    waitingQueue.broadcast();
 }
 
+
+//bed.cpp
+#include <stdio.h>
+#include <cassert>
+
+
+#define DEBUG_PRINT(errmsg) /*std::cerr << errmsg << std::endl*/ //DEBUG
+
+Bed::Bed() {
+    const int SHARED_WITH_THREADS = 0;
+    if (sem_init(&bed, SHARED_WITH_THREADS, 0)) perror("Bed ctor");
+}
+
+Bed::~Bed() {
+    if (sem_destroy(&bed)) perror("Bed dtor");
+}
+
+void Bed::registerSleeper() {
+    //if (bedPrepared) throw BedAlreadyOccupied();
+    //bedPrepared = true;
+	sleepers++;
+}
+
+bool Bed::sleep() {
+    //if (!bedPrepared) return false;
+    if (sem_wait(&bed)) perror("Bed::sleep");
+    return true;
+}
+
+void Bed::wake() {
+	sleepers--;
+    //if (!bedPrepared) throw BedUnoccupied();
+    //bedPrepared = false;
+
+    //wake whoever is in the bed
+    if (sem_post(&bed)) perror("Bed::wake sem_post");
+
+    //check if woke someone or if bed was unoccupied
+    int semaphoreCount;
+    if (sem_getvalue(&bed, &semaphoreCount)) perror("Bed::wake sem_getvalue");
+    //assert(semaphoreCount >= 0);
+    
+    //DEBUG_PRINT("Thread with id "<<gettid()<<" posted to bed, leaving semaphoreCount = "<<semaphoreCount);
+    
+
+    //if bed was empty, decrement it back to 0
+    /*if (semaphoreCount > 0) {
+        assert(semaphoreCount == 1);
+        if (sem_wait(&bed)) perror("Bed::wake sem_wait");
+    }*/ //commented out to preserve bed wake status if sleeper about to go to sleep
+}
+
+int Bed::getSleepersCount(){
+    /*int semaphoreCount;
+    if (sem_getvalue(&bed, &semaphoreCount)) perror("Bed::wake sem_getvalue");
+    DEBUG_PRINT("getSleepersCount returning "<<semaphoreCount);
+    return semaphoreCount;*/
+    return sleepers;
+}
+
+void Bed::awakenAll(){
+	DEBUG_PRINT("Awakening all sleepers, with getSleepersCount() = "<<getSleepersCount());
+	for (int waiters = getSleepersCount(); waiters>0; waiters--) {
+		wake();
+		DEBUG_PRINT("Awakening sleeper, getSleepersCount() = "<<getSleepersCount());
+	}
+}
+
+
+//ConditionVariable.cpp
+
+ConditionVariable::ConditionVariable(Mutex *lock) : lock(lock) {}
+
+void ConditionVariable::cond_wait() {
+    if (!(lock->isLocked())) throw LockNotAcquired(); //didn't follow usage instructions
+	
+	//DEBUG_PRINT("Thread with id " << gettid() << " waiting...");
+	
+    bed.registerSleeper();
+    //_isWaiting = true; //log that a process is sleeping here
+    lock->unlock();
+    bed.sleep(); //go to sleep unless signaled in the meanwhile to awaken
+
+    //after reawakening, reacquire lock
+    lock->lock();
+    //DEBUG_PRINT("Thread with id " << gettid() << " awoke!");
+}
+
+void ConditionVariable::cond_signal() {
+    if (!(lock->isLocked())) throw LockNotAcquired();
+    //DEBUG_PRINT("Thread with id " << gettid() << " signalling");
+    //_isWaiting = false;
+    bed.wake();
+}
+
+void ConditionVariable::broadcast(){
+	//assert(lock->isLocked());
+	DEBUG_PRINT("Broadcasting");
+	bed.awakenAll();
+}
+
+bool ConditionVariable::isWaiting() const {
+    return _isWaiting;
+}
+
+void ConditionVariable::setLock(Mutex *lock) {
+    ConditionVariable::lock = lock;
+}
+
+
+//mutex.cpp
+#include <stdio.h>
+
+#define DEBUG_PRINT(errmsg) /*std::cerr << errmsg << std::endl*/ //DEBUG
+
+Mutex::Mutex() {
+    if (pthread_mutex_init(&mutex, NULL)) perror("Mutex::ctor");
+}
+
+Mutex::~Mutex() {
+    if (pthread_mutex_destroy(&mutex)) perror("Mutex::ctor");
+}
+
+void Mutex::lock() {
+    if (pthread_mutex_lock(&mutex)) perror("Mutex::lock");
+    //DEBUG_PRINT("Thread with id " << gettid() << " acquired lock...");
+    _isLocked = true;
+}
+
+void Mutex::unlock() {
+    _isLocked = false;
+    //DEBUG_PRINT("Thread with id " << gettid() << " released lock.");
+    if (pthread_mutex_unlock(&mutex)) perror("Mutex::unlock");
+}
+
+bool Mutex::isLocked() {
+    /*if (!pthread_mutex_trylock(&mutex)) {
+		unlock();
+		return false;
+	};
+	return true;*/ return true;//DEBUG
+}
--- Barrier.h
+++ Barrier.h
@@ -5,16 +5,131 @@
 #ifndef BARRIER_H_
 #define BARRIER_H_
 
-#include "SyncPrimitives/ConditionVariable.h"
+
+//"Mutex.h"
+
+#include <pthread.h>
+
+class Mutex {
+private:
+    pthread_mutex_t mutex;
+    bool _isLocked = false;
+
+public:
+    Mutex();
+
+    ~Mutex(); //NOTE: destroys lock
+
+    void lock();
+
+    void unlock();
+
+    bool isLocked();
+};
+
+
+//"Bed.h"
+
+#include <semaphore.h>
+//"../BarrierException.h"
+
+#include <exception>
+
+class BarrierException : public std::exception {
+};
+
+class BedAlreadyOccupied : BarrierException {
+};
+
+class BedUnoccupied : BarrierException {
+};
+
+class LockNotAcquired : BarrierException {
+};
+
+
+//Not thread safe
+class Bed {
+private:
+	unsigned int sleepers = 0;
+    sem_t bed;
+    bool bedPrepared = false;
+
+public:
+    Bed(); //initializes bed with 0
+    ~Bed(); //destroys bed
+
+    //DESCRIPTION: start listening for signal but don't go to sleep yet
+    //THROWS: BedAlreadyOccupied
+    void registerSleeper();
+
+    //DESCRIPTION: puts thread to sleep until awakened by another thread by call to wake()
+    //USAGE: use after call to registerSleeper() or else will stay awake
+    //RETURN VALUE: true if went to sleep and then woken up, false if bed was unregistered
+    bool sleep();
+
+    //DESCRIPTION: wake thread sleeping on this bed (if exists) and unregister bed (either way)
+    //USAGE: use after sleeper is registered
+    //THROWS: BedUnoccupied
+    void wake();
+    
+    int getSleepersCount();
+    
+    void awakenAll();
+};
+
+
+//ConditionVariable.h
+class ConditionVariable {
+private:
+    Mutex *lock = nullptr;
+
+public:
+    void setLock(Mutex *lock);
+
+private:
+    bool _isWaiting = false;
+    Bed bed = Bed();
+
+public:
+    explicit ConditionVariable(Mutex *lock); //ctor
+    ConditionVariable() = default;
+
+    //DESCRIPTION: unlocks lock and goes to sleep *atomically*, then
+    //  (separately) reacquires lock when waking up
+    //USAGE: use after acquiring lock
+    //THROWS: LockNotAcquired
+    //SHOULDN'T THROW: BedAlreadyOccupied
+    void cond_wait();
+
+    //DESCRIPTION: wakes sleeping thread (if exists)
+    //USAGE: use after acquiring lock
+    //THROWS: LockNotAcquired
+    //SHOULDN'T THROW: BedUnoccupied
+    void cond_signal();
+
+    //returns if there is currently a thread sleeping on this ConditionVariable
+    bool isWaiting() const;
+    
+    //wake everyone
+    void broadcast();
+};
+
+
+
 #include <vector>
 #include <semaphore.h>
+#include <stdbool.h>
+#include <memory>
 
 class Barrier {
 private:
+    ConditionVariable waitingQueue = ConditionVariable(); //holds all processes waiting for queue to empty out prior to reuse
     const unsigned int numOfThreads;
     unsigned int nextEmptyBed = 0;
     Mutex barrierAccessLock = Mutex();
     std::vector<ConditionVariable> cond_beds; //vector of (numOfThreads - 1) ConditionVariables
+    bool _isOpen = true;
 
 public:
     explicit Barrier(unsigned int num_of_threads); //not thread-safe (doesn't need to be)
@@ -24,6 +139,10 @@ public:
     //USAGE: after acquiring barrierAccessLock
     //THROWS: LockNotAcquired
     bool isFull();
+    
+    //DESCRIPTION: returns if barrier is empty and next allows threads to enter barrier
+    //THROWS: LockNotAcquired
+    bool isOpen();
 
     //DESCRIPTION: resets Barrier so it is ready for reuse.
     //IMPLEMENTATION: zeroes nextEmptyBed
@@ -33,7 +152,12 @@ public:
     //SHOULDN'T THROW: LockNotAcquired, BedAlreadyOccupied, BedUnoccupied
     void wait();
 
-    unsigned int waitingThreads() { return nextEmptyBed; }
+    unsigned int waitingThreads() {	
+	barrierAccessLock.lock();
+ 	unsigned int result = nextEmptyBed; 
+    	barrierAccessLock.unlock();
+	return result;
+    }
 };
 
 
